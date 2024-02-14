067f99afde6e799fe0459ac9d5a1e00ca5175daa
Eecodev <mnasser.bbk@gmail.com>
2024-02-14 17:08:27 +0100

    homepage scaffolding

---
+++ src/components/RestaurantCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="card">
+        <img :src="`${store.imgUrl}${restaurant.image}`" class="card-img-top" :alt="restaurant.name">
+        <div class="card-body">
+            <h5 class="card-title">{{ restaurant.name }}</h5>
+            <p class="card-text">{{ restaurant.description }}</p>
+            <router-link :to="{name:'single-restaurant', params: {slug: restaurant.slug}}" class="btn btn-primary">View</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+import {store} from "../store.js";
+    export default {
+        name: 'RestaurantCard',
+        props: ['restaurant'],
+        data(){
+            return {
+                store
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,8 @@
 import {reactive} from 'vue';
 
 export const store = reactive({
-  apiUrl: 'http://127.0.0.1:8000/api/',
+  apiUrl: 'http://localhost:8000/api/',
+  imgUrl: 'http://localhost:8000/storage/',
+  restaurants: [],
+  cuisines: []
 })
