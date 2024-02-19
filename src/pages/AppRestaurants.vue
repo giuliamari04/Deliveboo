0f57a3eb52ed0a92101f933b6272d6975e66d5db
matteobonelli <matteobonelli08@gmail.com>
2024-02-19 19:35:28 +0100

    now copying url opens the correct page with query

--- src/pages/AppRestaurants.vue
+++ src/pages/AppRestaurants.vue
@@ -1,34 +1,40 @@
 <template>
     <div class="wrapper">
-            <!-- main content -->
-            <div class="container my-container">
-                <h1>Restaurants List</h1>
-                <div class="menu">
-                    <ul class="list-unstyled">
-                        <li>
-                            <span>Esplora le cucine</span>
-                        </li>
-                        <li class="d-flex flex-wrap">
-                            <div v-for="(cuisine, index) in store.cuisines" :key="index" class="d-flex flex-nowrap">
-                                <input type="checkbox" class="custom-checkbox" :id="cuisine.id" :value="cuisine.name" v-model="selectedCuisines">
-                                <label :for="'cuisine' + index" class="mx-2" >{{ cuisine.name }}</label>
-                            </div>
-                        </li>
-                    </ul>
-                    <button class="btn btn-lightgreen mb-4" @click="getAllRestaurantsFiltered()">Invia</button>
-                </div>
+        <!-- main content -->
+        <div class="container my-container">
+            <h1>Lista Ristoranti</h1>
+            <div class="alert alert-danger" v-if="store.restaurants.length <= 0">Non ci sono ristoranti con queste tipologie
+            </div>
+            <div class="menu">
+                <ul class="list-unstyled">
+                    <li>
+                        <span>Esplora le cucine</span>
+                    </li>
+                    <li class="d-flex flex-wrap">
+                        <div v-for="(cuisine, index) in store.cuisines" :key="index" class="d-flex flex-nowrap">
+                            <input type="checkbox" class="custom-checkbox" :id="cuisine.id" :value="cuisine.name"
+                                v-model="selectedCuisines">
+                            <label :for="'cuisine' + index" class="mx-2">{{ cuisine.name }}</label>
 
-                <div v-if="selectedCuisines">
-                    <div class="badge text-bg-success" v-for="cuisine in cuisineBadges">
-                        {{ cuisine }}
-                    </div>
+                        </div>
+                    </li>
+                </ul>
+                <button class="btn btn-lightgreen mb-4" @click="getAllRestaurantsFiltered(selectedCuisines)">Invia</button>
+
+            </div>
+
+            <div v-if="selectedCuisines">
+                <div class="badge text-bg-success" v-if="Array.isArray(cuisineBadges)" v-for="cuisine in cuisineBadges">
+                    {{ cuisine }}
                 </div>
-                <div class="row g-4">
-                    <div class="col-12 col-md-4 col-lg-3" v-for="restaurant in store.restaurants" :key="restaurant.id">
-                        <RestaurantCard :restaurant="restaurant" />
-                    </div>
+                <div class="badge text-bg-success" v-else>{{ cuisineBadges }}</div>
+            </div>
+            <div class="row g-4">
+                <div class="col-12 col-md-4 col-lg-3" v-for="restaurant in store.restaurants" :key="restaurant.id">
+                    <RestaurantCard :restaurant="restaurant" />
                 </div>
             </div>
+        </div>
     </div>
 </template>
 
@@ -57,12 +63,12 @@ export default {
                 console.log('error', err);
             })
         },
-        getAllRestaurantsFiltered() {
+        getAllRestaurantsFiltered(selectedCuisines) {
             this.cuisineBadges = ""
             let params = {};
 
-            if (this.selectedCuisines.length > 0) {
-                params.cuisines = this.selectedCuisines;
+            if (selectedCuisines.length > 0) {
+                params.cuisines = selectedCuisines;
             }
 
             axios.get(this.store.apiUrl + 'restaurants', { params })
@@ -73,66 +79,81 @@ export default {
                 .catch((err) => {
                     console.log('error', err);
                 });
-            this.cuisineBadges = this.selectedCuisines;
-            this.$router.push({ query: { cuisines: this.selectedCuisines } });
+            this.cuisineBadges = selectedCuisines;
+            this.$router.push({ query: { cuisines: selectedCuisines } });
         },
-        // getAllCuisines(){
-        //     axios.get(`${this.store.apiUrl}cuisines`).then((res)=> {
-        //         console.log(res.data);
-        //     })
-        // }
     },
     created() {
-        this.getAllRestaurants();
+        if (this.$route.query.cuisines) {
+            this.getAllRestaurantsFiltered(this.$route.query.cuisines);
+        } else {
+            this.getAllRestaurants();
+        }
         store.cartOpen = true
         // this.getAllCuisines();
+    },
+    mounted() {
+
+
     }
 }
 </script>
     
 <style lang="scss" scoped>
 @import '../assets/style/partials/variables';
-    .wrapper{
-        width: 100%;
-	    min-height: 100vh;
-    }
-    .title, .menu {
-        font-weight: 500;
-        text-transform: uppercase;
-        margin-bottom: 10px;
-    }
-    .menu{
-        border: 1px solid $lightgreen;
-        border-radius: 10px;
-    }
-    .name{
-        font-weight: 500;
-    }
-    .menu ul li{
-        list-style: none;
-        margin-bottom: 5px;
-    }
-    .menu ul li{
-        display: flex;
-        align-items: center;
-        gap: 10px;
-        text-decoration: none;
-        padding: 12px 8px;
-        border-radius: 8px;
-    }
-    span, .btn-lightgreen{
-        color: $lightgreen;
-    }
-    .btn-lightgreen{
-        border: 1px solid $lightgreen;
-        margin-left: 8px;
-    }
-    .btn-lightgreen:hover{
-        background-color: $lightgreen;
-        color: white;
-    }
-    .custom-checkbox input[type="checkbox"] {
-    display: none; 
+
+.wrapper {
+    width: 100%;
+    min-height: 100vh;
+}
+
+.title,
+.menu {
+    font-weight: 500;
+    text-transform: uppercase;
+    margin-bottom: 10px;
+}
+
+.menu {
+    border: 1px solid $lightgreen;
+    border-radius: 10px;
+}
+
+.name {
+    font-weight: 500;
+}
+
+.menu ul li {
+    list-style: none;
+    margin-bottom: 5px;
+}
+
+.menu ul li {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    text-decoration: none;
+    padding: 12px 8px;
+    border-radius: 8px;
+}
+
+span,
+.btn-lightgreen {
+    color: $lightgreen;
+}
+
+.btn-lightgreen {
+    border: 1px solid $lightgreen;
+    margin-left: 8px;
+}
+
+.btn-lightgreen:hover {
+    background-color: $lightgreen;
+    color: white;
+}
+
+.custom-checkbox input[type="checkbox"] {
+    display: none;
 }
 
 .custom-checkbox label {
@@ -154,11 +175,11 @@ export default {
     background-color: #fff;
 }
 
-.custom-checkbox input[type="checkbox"]:checked + label:before {
-    background-color: $lightgreen; 
+.custom-checkbox input[type="checkbox"]:checked+label:before {
+    background-color: $lightgreen;
 }
 
-.custom-checkbox input[type="checkbox"]:checked + label:after {
+.custom-checkbox input[type="checkbox"]:checked+label:after {
     content: '\2713';
     font-size: 14px;
     position: absolute;
@@ -166,7 +187,8 @@ export default {
     left: 4px;
     color: #fff;
 }
-    .my-container{
-        width: 100%;
-    }
+
+.my-container {
+    width: 100%;
+}
 </style>
