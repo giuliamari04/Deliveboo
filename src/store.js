582228fe63bb48656f7004e5e31ab3d610c6b6e6
giuliamari04 <giuliamari04@gmail.com>
2024-02-22 19:06:02 +0100

    fix

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { reactive } from 'vue';
 
 export const store = reactive({
   apiUrl: 'http://localhost:8000/api/',
-  imgUrl: 'http://[::1]:5173/storage/app/public/',
+  imgUrl: 'http://localhost:8000/storage/',
   restaurants: [],
   cuisines: [],
   cart: [],
