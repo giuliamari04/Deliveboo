fe671795360354674cedcd6c8c0d631ee13635a9
Eecodev <mnasser.bbk@gmail.com>
2024-02-22 16:27:07 +0100

    hero comp added

---
+++ src/components/Hero.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="hero">
+        <div class="hero-content">
+            <h1>I piatti dei ristoranti che ami e la spesa, a domicilio</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Hero'
+    }
+</script>
+
+<style lang="scss" scoped>
+@use "../assets/style/partials/variables";
+
+    #hero{
+        position: relative;
+        height: 100vh;
+        background-image: url('/public/img/hero.jpeg');
+        background-size: cover;
+        background-position: center;
+        text-align: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .hero-content{
+        max-width: 600px;
+        margin: 0 auto;
+    }
+    .hero-content h1{
+        font-size: 3rem;
+        margin-bottom: 20px;
+        color: black;
+    }
+</style>
--- src/pages/AppRestaurants.vue
+++ src/pages/AppRestaurants.vue
@@ -1,6 +1,9 @@
 <template>
+    <div>
+        <Hero/>
+    </div>
     <div class="d-flex justify-content-between align-items-center my-3">
-        <h1>Restaurants List</h1>
+        <h1>I nostri ristoranti</h1>
         <select name="cuisine" id="cuisine">
             <option value="">--Choose Cuisine</option>
             <option :value="cuisine.id" v-for="(cuisine, index) in store.cuisines" :key="index">{{ cuisine.name }}</option>
@@ -18,10 +21,12 @@
 import {store} from "../store.js";
 import axios from "axios";
 import RestaurantCard from "../components/RestaurantCard.vue";
+import Hero from "@/components/Hero.vue";
     export default {
         name: "AppRestaurants",
         components: {
-            RestaurantCard
+            RestaurantCard,
+            Hero
         },
         data(){
             return{
