b7fd82bf1536fddfd7e9cddbbcb15f327c8b336e
Eecodev <mnasser.bbk@gmail.com>
2024-02-23 16:40:06 +0100

    updated

--- src/store.js
+++ src/store.js
@@ -4,6 +4,7 @@ export const store = reactive({
   apiUrl: 'http://localhost:8000/api/',
   imgUrl: 'http://localhost:8000/storage/',
   restaurants: [],
+  products: [],
   cuisines: [],
   cart: [],
   cartOpen: false,
